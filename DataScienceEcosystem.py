bcd78be0e7dbb8f1d968c1a47ffb5fd1752a4b6d
Olokede <142906995+Olokede@users.noreply.github.com>
2023-08-23 11:17:23 +0100

    Add files via upload
    Final Assignment of the second course, Tools for Data Science

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# ## In this notebook, Data Science Tools and Ecosystem are summarized
+
+# ## **Objectives** :
+# + List popular languages for Data Science
+# + Jupyter notebook and Jupyterlab
+# + R and R Studio
+# + Libraries for Data Science 
+# 
+
+# ### Some of the popular languages that Data Scientists use are:
+# 1. Python
+# 2. R
+# 3. C++
+# 4. Java
+# 5. Javascript 
+
+# ### Some of the commonly used libraries used by Data Scientists include:
+# 1. Numpy
+# 2. Pandas
+# 3. Matplotlib
+# 4. pyTorch
+
+# | Data Science Tools |
+# -------------------------------
+# | Jupyter notebook |
+# | R Studio |
+# | JupyterLab |
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+
+# In[1]:
+
+
+# This a simple arithmetic expression to mutiply then add integers
+
+(3*4)+5
+
+
+# In[2]:
+
+
+#This will convert 200 minutes to hours by diving by 60
+
+200 / 60
+
+
+# ## Author
+# Oluwatomi Olokede
+
+# In[ ]:
+
+
+
+
